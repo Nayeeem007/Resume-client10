d4cd61a050f60b654f757a85f51aa5001f3ff68b
Md. Abu Nayeem <niloy1882@gmail.com>
2023-05-05 22:12:40 +0600

    resister section added

--- src/pages/resister/Resister.jsx
+++ src/pages/resister/Resister.jsx
@@ -1,89 +1,24 @@
-import React, { useContext, useState } from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import NavBar from './Navbar';
+import { Outlet } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProvider';
 
-const Resister = () => {
-    const [error,setError]=useState('')
-    const [success,setSuccess]=useState('')
-    const {createUser}=useContext(AuthContext);
-    const handleResister=event=>{
-        event.preventDefault();
-        const form = event.target;
-        const name= form.name.value;
-        const photo=form.photo.value;
-        const email=form.email.value;
-        const password=form.password.value;
-        
-      
-
-        createUser(email, password)
-        .then(result=>{
-            const createdUser= result.user;
-            console.log(createdUser);
-        })
-        .catch(error=>{
-            setError(error.message);
-        })
-        
-        
-            if(password.length<6){
-                setError('Put six char') 
-                setSuccess('')    
-        }
-       
-        else{
-            setSuccess('Logged in successfully')
-            setError('')
-        }
-    }
-
-    
+const HomePage = () => {
+    const {loading}=useContext(AuthContext)
+   
 
     return (
-        <Container className='mx-auto,mt-5 w-25'>
-            <h2 className='text-success text-center'>Please Resister</h2>
-             <Form onSubmit={handleResister}>
-      <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>Your Name</Form.Label>
-        <Form.Control type="name" name='name' placeholder="Enter name" required />
-        
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" name='email' placeholder="Enter email" required />
-        
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPhoto">
-        <Form.Label>Your Photo</Form.Label>
-        <Form.Control type="text" name='photo' placeholder="Photo url" required />
-        
-      </Form.Group>
+        <div>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name='password'   placeholder="Password" required/>
-      </Form.Group>
-     
-      {error ? <Form.Text className="text-danger">
-          {error}
-        </Form.Text>
-    : <Form.Text className="text-success">
-    {success}
-    </Form.Text>     
-    } <br />
-      <Button className='mt-3' variant="primary" type="submit">
-        Submit
-      </Button> <br />
-
-      <Form.Text >
-            Already have an account? please <Link to='/login'> login</Link>
-        </Form.Text>
-      
-      
-    </Form>
-        </Container>
+            <NavBar></NavBar>
+            {loading && <div className="d-flex justify-content-center">
+  <div className="spinner-border" role="status">
+    <span className="visually-hidden">Loading...</span>
+  </div>
+</div>}
+            <Outlet></Outlet>
+        </div>
     );
 };
 
-export default Resister;
+export default HomePage;
