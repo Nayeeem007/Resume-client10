535613e5f7329a5ddd9e4e4f2770ddcdb368d595
Md. Abu Nayeem <niloy1882@gmail.com>
2023-05-05 22:05:29 +0600

    homepage jsx file spinner added

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import NavBar from './Navbar';
 import { Outlet } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const HomePage = () => {
+    const {loading}=useContext(AuthContext)
+   
 
     return (
         <div>
+
             <NavBar></NavBar>
+            {loading && <div className="d-flex justify-content-center">
+  <div className="spinner-border" role="status">
+    <span className="visually-hidden">Loading...</span>
+  </div>
+</div>}
             <Outlet></Outlet>
         </div>
     );
 };
-
 
 export default HomePage;
