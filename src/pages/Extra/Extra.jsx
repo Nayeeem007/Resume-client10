dae03cada44c83e47149363a12bdbf3d855f73a0
Md. Abu Nayeem <niloy1882@gmail.com>
2023-05-05 22:07:46 +0600

    Extra section added

--- src/pages/Extra/Extra.jsx
+++ src/pages/Extra/Extra.jsx
@@ -1,21 +1,69 @@
-import React from 'react';
-import Marquee from "react-fast-marquee";
+import React, { useContext, useState } from 'react';
+import { Container, Nav, Button, Navbar } from 'react-bootstrap';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import {  NavLink } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProvider';
+import './Navbar.css';
+
+const NavBar = () => {
+
+const {user, logout}= useContext(AuthContext);
+
+const handleLogout=()=>{
+  logout()
+  .then()
+  .catch(error=>{
+    console.log(error);
+})
+}
+//hove=====
+const renderTooltip = (props) => (
+  <Tooltip id="button-tooltip" {...props}>
+    {user.displayName}
+  </Tooltip>
+);
+//=======
 
 
-const Extra = () => {
     return (
-        <div className='container my-5'>
-            <h2 className='text-success text-center my-3'>We Serve The Best Food</h2>
-            <Marquee speed={100}>
-                <img className='m-img w-50' src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141353.jpg" alt="" />
-                <img className='m-img w-50' src="https://img.freepik.com/free-photo/flat-lay-batch-cooking-composition_23-2148765597.jpg" alt="" />
-                <img className='m-img w-50' src="https://media.istockphoto.com/id/922783734/photo/assorted-indian-recipes-food-various.jpg?s=612x612&w=0&k=20&c=p8DepvymWfC5j7c6En2UsQ6sUM794SQMwceeBW3yQ9M=" alt="" />
-                <img className='m-img w-50' src="https://media.istockphoto.com/id/1005390222/photo/chicken-tikka-masala-spicy-curry-meat-food-with-rice-and-naan-bread.jpg?s=612x612&w=0&k=20&c=UPKJT67Bn-2ZL1TakWLK9IhpAOIMgryGhLwoHRsP8ZI=" alt="" />
-                <img className='m-img w-50' src="https://media.istockphoto.com/id/618457124/photo/butter-chicken-served-with-homemade-indian-naan-bread.jpg?s=612x612&w=0&k=20&c=7FoiHoDtocfPvQIaRFfFani4e5lkfMTNl_619rTTh4g=" alt="" />
-
-            </Marquee>
-        </div>
+       <Container>
+         <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand className='text-success fw-bold fs-1'>Cooking Master</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0 "
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            
+            
+            
+            
+          </Nav>
+          {
+            user ? <OverlayTrigger
+            placement="right"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip}
+          >
+            <img  className='rounded-circle p-img mx-3 hover-overlay' src={user ?.photoURL} alt="" />
+          </OverlayTrigger> : 
+            <img className='rounded-circle p-img mx-3' src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
+          }
+          <NavLink className='pe-3 text-decoration-none fw-semibold' to="/">Home</NavLink>
+            { user? <Button onClick={handleLogout} className='px-3 btn-success mx-3 text-decoration-none' to="/logout">Logout</Button>:
+              <NavLink className='pe-3 text-decoration-none fw-semibold' to="/login">Login</NavLink>
+            }
+            <NavLink className='pe-3 text-decoration-none fw-semibold' to="/resister">Resister</NavLink>
+            <NavLink className='text-decoration-none fw-semibold'  to="/blogs">Blogs</NavLink>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+       </Container>
     );
 };
 
-export default Extra;
+export default NavBar;
